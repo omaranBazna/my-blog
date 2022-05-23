4beb430ca40436d91e486d74a730a2592693625b
omaranBazna <omran4d@gmail.com>
2022-05-22 22:20:22 -0400

    retrieve data from firebase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection, getDocs, onSnapshot
+} from 'firebase/firestore';
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBHRyuOeJenmiEstwz8O2AyQloxkMVg1ck",
+  authDomain: "myblog-8547c.firebaseapp.com",
+  projectId: "myblog-8547c",
+  storageBucket: "myblog-8547c.appspot.com",
+  messagingSenderId: "1056969993052",
+  appId: "1:1056969993052:web:c620a5e9d2c5fc2aa6ef99"
+};
+const app = initializeApp(firebaseConfig);
+const db=getFirestore();
+const colRef=collection(db,"books");
+getDocs(colRef).then((snpashot)=>{
+  snpashot.docs.map((doc)=>{
+   console.log({...doc.data(),id:doc.id});
+  })
+}).catch(err=>{
+  console.log(err);
+})
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {initializeApp} from "firebase/app";
+import{
+  getFireStore,
+  collection
+} from 'firebase/firestore'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -15,3 +19,11 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+const firebaseConfig = {
+  apiKey: "AIzaSyBHRyuOeJenmiEstwz8O2AyQloxkMVg1ck",
+  authDomain: "myblog-8547c.firebaseapp.com",
+  projectId: "myblog-8547c",
+  storageBucket: "myblog-8547c.appspot.com",
+  messagingSenderId: "1056969993052",
+  appId: "1:1056969993052:web:c620a5e9d2c5fc2aa6ef99"
+};
